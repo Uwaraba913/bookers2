415776c3a72bbc44acc5a319b33cc6d7b0c74023
Uwaraba913 <metalcoola5963@gmail.com>
2021-08-16 11:58:37 +0000

    8-16

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_08_15_054431) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "user_image"
+    t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
