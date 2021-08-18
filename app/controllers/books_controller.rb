8a3e1d12f319244fabb023a2f121ada161d26161
Uwaraba913 <metalcoola5963@gmail.com>
2021-08-18 13:03:41 +0000

    8-18

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,28 @@
 class BooksController < ApplicationController
+  before_action :check_user, only: [:edit, :destroy, :update]
+
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     redirect_to book_path(@book.id), notice: 'You have created book successfully.'
+    else
+     @books = Book.all
+     @user =  current_user
+     render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
+    @user = current_user
   end
 
   def edit
@@ -21,8 +31,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -35,5 +48,11 @@ class BooksController < ApplicationController
 
   def book_params
       params.require(:book).permit(:title, :body)
+  end
+
+  def check_user
+    unless Book.find(params[:id]).user_id.to_i == current_user.id
+      redirect_to books_path
+    end
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ff8d5460077fa1395eeffe82d384c87c0576737e9b56a3a343d5e38abdd44a37739f7d790f54b2ff974293748cf31f7d5569714af70c06f6b421f99a89802698'
