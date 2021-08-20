452b2b2219e31521eb4e3312b80c254c6b536d74
Uwaraba913 <metalcoola5963@gmail.com>
2021-08-20 08:27:16 +0000

    8-19

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
-    @user = current_user
+    @user = @book.user
   end
 
   def edit
